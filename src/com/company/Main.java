96f0c659f5885ede34fa59a6a1f7a23a765a686a
Fernando  Spadea <fs279@pvhs-lab112-d10.pvsd.org>
2016-09-08 14:29:04 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare variable you:
+        //String or int or double
+        String s;
+        s = "name";//initialize
+        String s2 = "nickname";
+        System.out.println(s);
+        //Naming rules of variables
+        //1. if, else, void, public, etc.
+        //avoid all reserved words
+        // 2. can't start with a number
+        // 1asdasfaf is not good!
+        // 3. Special characters $#!
+        // 4. Spaces are evil!
+        // First Name
+        // firstName first_name
+        // 5. reuse a variable name
+        //String vs int
+        //int vs double
+        int a = 45;
+        double b = 3;
+        double sum = a + b;
+
+        a = 2_342_342;
+        int E,d = 0;
+        int sometime;
+        int thissssss = 0;
+        String samthing = "";
+        //String object pieces of it
+        //Strings have methods.
+
+    }
+}
